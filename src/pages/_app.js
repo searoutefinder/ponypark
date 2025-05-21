ae8bcc332e1fed9db770bd46fa627c92b9ad0678
Tamas Hajdu <info@ezmapdesign.com>
2025-05-21 09:51:49 +0200

    Added Google Analytics Tracking

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import Script from 'next/script';
 import NProgress from "nprogress";
 import "nprogress/nprogress.css"; // Import styles
 import '../styles/globals.css';
+
+const GA_MEASUREMENT_ID = 'G-9Z79CK541L';
 
 function MyApp({ Component, pageProps }) {
   const [isPageTransitioning, setIsPageTransitioning] = useState(false);
@@ -48,7 +51,36 @@ function MyApp({ Component, pageProps }) {
     };
   }, [router]);
 
-  return <div className={`${isPageTransitioning ? 'fade-exit' : 'fade-enter'} transition-opacity duration-500`}>   
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', GA_MEASUREMENT_ID, {
+        page_path: url,
+      });
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => router.events.off('routeChangeComplete', handleRouteChange);
+  }, [router.events]);  
+
+  return <div className={`${isPageTransitioning ? 'fade-exit' : 'fade-enter'} transition-opacity duration-500`}>
+    <Script
+      strategy="afterInteractive"
+      src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+    />
+    <Script
+      id="gtag-init"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+      }}
+    />       
     <Component {...pageProps} />
   </div>
 }
