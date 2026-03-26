078da9b67a465a1275b49054b642868e8385a615
Tamas Hajdu <info@ezmapdesign.com>
2026-03-26 11:45:57 +0100

    Added title to the question modal

--- src/components/QuestionModal/QuestionModal.js
+++ src/components/QuestionModal/QuestionModal.js
@@ -5,7 +5,7 @@ const QuestionModal = ({data, buttonText, onButtonClick}) => {
 
 <div className="relative z-50" aria-labelledby="modal-title" role="dialog" aria-modal="true">
     <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0 bg-[rgba(0,0,0,0.5)]">
+        <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0 bg-[rgba(0,0,0,0.5)]">
 
             <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                 <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
@@ -13,18 +13,19 @@ const QuestionModal = ({data, buttonText, onButtonClick}) => {
                         <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">                    
                             <div className="mt-2">
                                 <div className="flex flex-col gap-2">
-                                <img 
+                                  <h1 className="text-lg text-center mb-5 text-gray-500">{data.treasure_title !== "" ? data.treasure_title : "Question"}</h1>
+                                  <img 
                                     src={data.treasure_pic_url}
-                                    className="w-full h-auto pb-5 rounded-lg"
-                                />
-                                <p 
+                                    className="w-full h-auto rounded-lg mb-5"
+                                  />
+                                  <p 
                                     className="text-sm text-gray-500 p-3"
                                     dangerouslySetInnerHTML={{ __html: data.treasure_question_nl }}
-                                />
-                                <p
+                                  />
+                                  <p
                                     className="text-sm text-gray-500 bg-[#fffded] p-3 rounded-lg"
                                     dangerouslySetInnerHTML={{ __html: data.treasure_question_de }}
-                                />
+                                  />
                                 </div>
                             </div>
                         </div>
